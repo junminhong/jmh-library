d21ac781ec0f1f741a211ec6538bff60b4e17107
junminhong <junminhong1110@gmail.com>
2024-12-19 14:44:07 +0800

    fix: tailwind config waring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './docs/**/*.md',
+    './blog/*.md'
+  ],
   theme: {
     extend: {},
   },
